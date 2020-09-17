3b09f87b67e686e06732e6f5e2d497365a729724
Jeff Klenzing <jklenzing@gmail.com>
2020-09-17 14:51:05 -0400

    MAINT: update instrument test class

--- pysatCDAAC/tests/test_instruments.py
+++ pysatCDAAC/tests/test_instruments.py
@@ -9,8 +9,8 @@ from pysat.tests.instrument_test_class import InstTestClass
 # Developers for instrument libraries should update the following line to
 # point to their own library package
 # e.g.,
-# instruments = generate_instrument_list(package=mypackage.instruments)
-instruments = generate_instrument_list(package=pysatCDAAC.instruments)
+# instruments = generate_instrument_list(inst_loc=mypackage.instruments)
+instruments = generate_instrument_list(inst_loc=pysatCDAAC.instruments)
 
 # The following lines apply the custom instrument lists to each type of test
 method_list = [func for func in dir(InstTestClass)
@@ -24,13 +24,14 @@ for method in method_list:
                  for j in range(0, Nargs)]
         # Add instruments from your library
         if 'all_inst' in names:
-            mark = pytest.mark.parametrize("name", instruments['names'])
+            mark = pytest.mark.parametrize("inst_name", instruments['names'])
             getattr(InstTestClass, method).pytestmark.append(mark)
         elif 'download' in names:
-            mark = pytest.mark.parametrize("inst", instruments['download'])
+            mark = pytest.mark.parametrize("inst_dict", instruments['download'])
             getattr(InstTestClass, method).pytestmark.append(mark)
         elif 'no_download' in names:
-            mark = pytest.mark.parametrize("inst", instruments['no_download'])
+            mark = pytest.mark.parametrize("inst_dict",
+                                           instruments['no_download'])
             getattr(InstTestClass, method).pytestmark.append(mark)
 
 
@@ -40,9 +41,9 @@ class TestInstruments(InstTestClass):
         """Runs before every method to create a clean testing setup."""
         # Developers for instrument libraries should update the following line
         # to point to their own library package, e.g.,
-        # self.package = mypackage.instruments
-        self.package = pysatCDAAC.instruments
+        # self.inst_loc = mypackage.instruments
+        self.inst_loc = pysatCDAAC.instruments
 
     def teardown(self):
         """Runs after every method to clean up previous testing."""
-        del self.package
+        del self.inst_loc
