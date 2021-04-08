7f5fe986e7af0ff89a1b73fd2416c49a63199761
Russell Stoneback <rstoneba@utdallas.edu>
2021-04-08 17:57:59 -0500

    STY: Removed empty file list warning, handled by pysat

--- pysatCDAAC/instruments/cosmic_gps.py
+++ pysatCDAAC/instruments/cosmic_gps.py
@@ -287,10 +287,6 @@ def list_files(tag=None, inst_id=None, data_path=None, format_str=None):
         file_list = pds.Series(file_list, index=index)
         return file_list
 
-    else:
-        logger.info('Found no files, check your path or download them.')
-        return pds.Series(None, dtype='object')
-
 
 def load(fnames, tag=None, inst_id=None, altitude_bin=None):
     """Load COSMIC GPS files.
