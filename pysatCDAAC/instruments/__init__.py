e9fad4116984635143bbb2b77055019f569f6011
Jeff Klenzing <jklenzing@gmail.com>
2021-10-21 17:29:30 -0400

    STY: clean up imports

--- pysatCDAAC/instruments/__init__.py
+++ pysatCDAAC/instruments/__init__.py
@@ -9,3 +9,6 @@ __all__ = ['cosmic_gps']
 
 for inst in __all__:
     exec("from pysatCDAAC.instruments import {x}".format(x=inst))
+
+# Remove dummy variable
+del inst
--- pysatCDAAC/tests/test_instruments.py
+++ pysatCDAAC/tests/test_instruments.py
@@ -12,8 +12,8 @@ import tempfile
 
 import pysat
 # Import the test classes from pysat
-from pysat.utils import generate_instrument_list
 from pysat.tests.instrument_test_class import InstTestClass
+from pysat.utils import generate_instrument_list
 
 # Make sure to import your instrument library here
 import pysatCDAAC
