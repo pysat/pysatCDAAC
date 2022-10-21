f9b5815575519b58aaf26525d0ec307ccc2aa6fe
Jeff Klenzing <jklenzing@users.noreply.github.com>
2022-10-21 15:32:03 -0400

    MIANT: update instrument test standards

--- pysatCDAAC/tests/test_instruments.py
+++ pysatCDAAC/tests/test_instruments.py
@@ -8,71 +8,31 @@ Imports test methods from pysat.tests.instrument_test_class
 
 import numpy as np
 import pytest
-import tempfile
 
 import pysat
 # Import the test classes from pysat
-from pysat.tests.instrument_test_class import InstTestClass
-from pysat.utils import generate_instrument_list
+from pysat.tests.classes import cls_instrument_library as clslib
 
 # Make sure to import your instrument library here
 import pysatCDAAC
 
 
 # Retrieve the lists of CDAAC instruments and testing methods
-instruments = generate_instrument_list(inst_loc=pysatCDAAC.instruments)
-
-method_list = [func for func in dir(InstTestClass)
-               if callable(getattr(InstTestClass, func))]
-
-# Search tests for iteration via pytestmark, update instrument list
-for method in method_list:
-    if hasattr(getattr(InstTestClass, method), 'pytestmark'):
-        # Get list of names of pytestmarks
-        Nargs = len(getattr(InstTestClass, method).pytestmark)
-        names = [getattr(InstTestClass, method).pytestmark[j].name
-                 for j in range(0, Nargs)]
-        # Add instruments from your library
-        if 'all_inst' in names:
-            mark = pytest.mark.parametrize("inst_name", instruments['names'])
-            getattr(InstTestClass, method).pytestmark.append(mark)
-        elif 'download' in names:
-            mark = pytest.mark.parametrize("inst_dict", instruments['download'])
-            getattr(InstTestClass, method).pytestmark.append(mark)
-        elif 'no_download' in names:
-            mark = pytest.mark.parametrize("inst_dict",
-                                           instruments['no_download'])
-            getattr(InstTestClass, method).pytestmark.append(mark)
+instruments = clslib.InstLibTests.initialize_test_package(
+    clslib.InstLibTests, inst_loc=pysatCDAAC.instruments)
 
 
-class TestInstruments(InstTestClass):
+class TestInstruments(clslib.InstLibTests):
     """Main class for instrument tests.
 
     Note
     ----
-    Uses class level setup and teardown so that all tests use the same
-    temporary directory. We do not want to geneate a new tempdir for each test,
-    as the load tests need to be the same as the download tests.
+    All standard tests, setup, and teardown inherited from the core pysat
+    instrument test class.
 
     """
 
-    def setup_class(self):
-        """Initialize the testing setup once before all tests are run."""
-        # Make sure to use a temporary directory so that the user's setup is not
-        # altered
-        self.tempdir = tempfile.TemporaryDirectory()
-        self.saved_path = pysat.params['data_dirs']
-        pysat.params.data['data_dirs'] = [self.tempdir.name]
-
-        # Assign the location of the Instrument sub-modules
-        self.inst_loc = pysatCDAAC.instruments
-
-    def teardown_class(self):
-        """Clean up downloaded files and parameters from tests."""
-        pysat.params.data['data_dirs'] = self.saved_path
-        self.tempdir.cleanup()
-        del self.inst_loc, self.saved_path, self.tempdir
-
+    @pytest.mark.second
     @pytest.mark.parametrize("inst_dict", [x for x in instruments['download']])
     @pytest.mark.parametrize("bin_num", [100, 200])
     def test_altitude_bin_keyword(self, inst_dict, bin_num):
