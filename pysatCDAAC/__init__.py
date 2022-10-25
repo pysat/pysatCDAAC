23c3147c3d0b987f97dbfdd8dcc25b27e83cd898
Jeff Klenzing <jklenzing@users.noreply.github.com>
2022-10-25 18:00:52 -0400

    MAINT: use pyproject.toml

--- pysatCDAAC/__init__.py
+++ pysatCDAAC/__init__.py
@@ -1,1 +1,12 @@
+"""Core library for pysatCDAAC.
+
+This is a library of `pysat` instrument modules and methods designed to support
+COSMIC instruments and missions archived at the CDAAC web portal.
+
+"""
+
+import pkg_resources
+
 from pysatCDAAC import instruments  # noqa F401
+
+__version__ = pkg_resources.get_distribution('pysatCDAAC').version
