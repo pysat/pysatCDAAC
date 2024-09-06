cea00f9018e7ed06309cd7922d9a2592e8d66b5f
Angeline Burrell <angeline.burrell@nrl.navy.mil>
2024-09-06 15:41:29 -0400

    DOC: updated header
    Added the pub release header to the code.

--- pysatCDAAC/instruments/methods/general.py
+++ pysatCDAAC/instruments/methods/general.py
@@ -1,4 +1,10 @@
 # -*- coding: utf-8 -*-
+# Full author list can be found in .zenodo.json file
+# DOI:10.5281/zenodo.3475493
+#
+# DISTRIBUTION STATEMENT A: Approved for public release. Distribution is
+# unlimited.
+# ----------------------------------------------------------------------------
 """Provides default routines for CDAAC instruments into pysat."""
 
 import os
